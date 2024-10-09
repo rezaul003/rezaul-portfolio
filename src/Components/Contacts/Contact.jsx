99d99ab42bf60321df9da9b0796195d3ba2cde76
rezaul003 <krezaul839@gmail.com>
2024-10-09 10:45:14 +0600

    contact complete

--- src/Components/Contacts/Contact.jsx
+++ src/Components/Contacts/Contact.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import './Contacts.css'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
     return (
         <div className='contact'>
-            <div className="getintouch">
+            <div className="contact-titile">
                 <h1>Get in touch</h1>
             </div>
-            
+
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's talk </h1>
+                    <p>I'm currently avaliable to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src={mail_icon} alt="" />
+                            <p>greatstackdev@gmail.com</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={call_icon} alt="" />
+                            <p>+772-825-524</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={location_icon} alt="" />
+                            <p>Dhaka, Bangladesh</p>
+                        </div>
+                    </div>
+                </div>
+
+                <form className="contact-right">
+                    <label htmlFor="">Your Name</label>
+                    <input type="text" placeholder='Enter Your Name' name='name' />
+                    <label htmlFor="">Your Email</label>
+                    <input type="email" placeholder='Enter Your Email' name='email' />
+                    <label htmlFor="">Write Your Message Here</label>
+                    <textarea name="message" rows="8" placeholder='Enter Your Message'></textarea>
+                    <button type='submit' className='contact-submit'>Submit Now</button>
+                </form>
+            </div>
+
         </div>
     );
 };
